a217c457c9ab2b662ef2fef6fd02c43916bcf8a7
hiren1999 <hiren.thinktanker@gmail.com>
2021-03-10 10:43:00 +0530

    auth done

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const flash = require("express-flash");
 
 // const MongoDbStore = require("connect-mongo")(session);
 
+const passport = require("passport");
+
 /* <---- Database connection  ----> */
 
 const url = "mongodb://localhost/pizza";
@@ -53,15 +55,23 @@ app.use(
     })
 );
 
+/* <---- Passport config  ----> */
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
-//assets
+/* <---- assets  ----> */
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 /* <---- Global Middleware  ----> */
 app.use((req, res, next) => {
     res.locals.session = req.session;
+    res.locals.user = req.user;
     next();
 });
 
